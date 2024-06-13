abd8f8f0696057e60613e995fcfa21e375d0da65
KarenS <channaru@icloud.com>
2024-06-12 22:08:27 -0700

    Added Java springboot files, dependencies

---
+++ rectangles/src/test/java/com/_6/rectangles/RectanglesApplicationTests.java
@@ -0,0 +1,13 @@
+package com._6.rectangles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RectanglesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
